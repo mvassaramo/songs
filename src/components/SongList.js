fd8557c1868c41bae4bbc6c6bc31c3a816add670
Maduri Vassaramo <maduri@vinterior.co>
2021-06-09 11:27:40 +0200

    add props from store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  
   render() {
     return(<div>Song List</div>)
   }
 }
 
+const mapStateToProps = (state) => {
+ return { songs: state.songs }
+}
 
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
 
